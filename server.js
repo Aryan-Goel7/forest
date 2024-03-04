125cf8dc1ac61bce950e273d0978c10751813cd2
Aryan Goel <aryangoel971@gmail.com>
2024-03-04 16:14:27 +0530

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const tagRouter = require("./routes/tags");
 const sessionRouter = require("./routes/sessions");
-const { authenticateUser } = require("./middleware/Auth");
 const router = express.Router();
 
 // router.use(authenticateUser);
